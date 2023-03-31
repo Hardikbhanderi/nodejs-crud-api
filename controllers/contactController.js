6ace107889d5bbd4d9a4435091e518a392e403f5
Hardik <bhanderi8642@gmail.com>
2023-03-31 11:03:59 +0530

    initial commit

---
+++ controllers/contactController.js
@@ -0,0 +1,35 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
+
+
+const getContacts = asyncHandler(async (req, res) => {
+    const contacts = await Contact.find();
+  res.status(200).json(contacts);
+});
+
+const createContact = asyncHandler(async (req, res) => {
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory !");
+  }
+  res.status(201).json({ message: "Create contact" });
+});
+const getContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+});
+const updateContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+});
+
+const deleteContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+});
+
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
